6a358e99f43da25dc890541e92877e02ec01fe4a
haseebahmed-deutics <haseeb.ahmed@deutics.com>
2023-02-17 00:26:16 +0500

    Added functionality to add todos in list

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import List
+from .forms import Listform
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    all_list_items=List.objects.all()
-    return render(request, 'home.html', {'active': 'home','things':all_list_items})
+    if request.method == 'POST':
+        form = Listform(request.POST or None)
+        if form.is_valid():
+            form.save()
+            all_list_items = List.objects.all()
+            messages.success(request,'Item has been added to List!')
+            return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
+
+    else:
+        all_list_items = List.objects.all()
+        return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
 
 
 def about(request):
