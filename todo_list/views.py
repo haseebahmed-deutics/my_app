9348b2de65aea9d9d905c601b7d9740b8df228c1
haseebahmed-deutics <haseeb.ahmed@deutics.com>
2023-02-16 21:11:44 +0500

    Adding templates

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    # return HttpResponse('<h1> home </h1>')
+    return render(request,'home.html')
+
+def about(request):
+    # return HttpResponse('<h1> about </h1>')
+    return render(request,'about.html')
+
