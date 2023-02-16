e8b201f28a75b628c67570c725788cbcdd97f521
haseebahmed-deutics <haseeb.ahmed@deutics.com>
2023-02-17 00:11:02 +0500

    Adding List Model and displaying list of todos

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import List
 
 # Create your views here.
 
 def home(request):
-    # return HttpResponse('<h1> home </h1>')
-    return render(request,'home.html')
+    all_list_items=List.objects.all()
+    return render(request, 'home.html', {'active': 'home','things':all_list_items})
+
 
 def about(request):
-    # return HttpResponse('<h1> about </h1>')
-    return render(request,'about.html')
-
+    return render(request, 'about.html', {'active': 'about'})
