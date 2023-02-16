cc73b096fd635c3f9dc492b636462900adf40d11
haseebahmed-deutics <haseeb.ahmed@deutics.com>
2023-02-17 01:02:30 +0500

    Adding Cross and Uncross items functionality

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/',views.about,name ='about')
+    path('about/', views.about, name='about'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('cross_off/<int:id>', views.cross_off, name='cross_off'),
+    path('uncross/<int:id>', views.uncross, name='uncross'),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import List
 from .forms import Listform
 from django.contrib import messages
+
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_list_items = List.objects.all()
-            messages.success(request,'Item has been added to List!')
+            messages.success(request, 'Item has been added to List!')
             return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
 
     else:
@@ -22,3 +23,32 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {'active': 'about'})
+
+
+def delete(request, id):
+    if request.method == 'GET':
+        obj = get_object_or_404(List, pk=id)
+        obj.delete()
+        messages.success(request, 'Item has been deleted from List!')
+        all_list_items = List.objects.all()
+        return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
+
+
+def cross_off(request, id):
+    if request.method == 'GET':
+        obj = get_object_or_404(List, pk=id)
+        obj.completed = True
+        obj.save()
+        messages.success(request, 'Item has been crossed_off successfully!')
+        all_list_items = List.objects.all()
+        return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
+
+
+def uncross(request, id):
+    if request.method == 'GET':
+        obj = get_object_or_404(List, pk=id)
+        obj.completed = False
+        obj.save()
+        messages.success(request, 'Item has been uncrossed successfully!')
+        all_list_items = List.objects.all()
+        return render(request, 'home.html', {'active': 'home', 'things': all_list_items})
